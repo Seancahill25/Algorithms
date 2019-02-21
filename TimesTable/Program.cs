c0c3717fc2c4a452582980865fc949f637c84d0f
Sean Cahill <seancahill25@gmail.com>
2019-02-20 23:22:31 -0500

    basic multiplication using a loop

---
+++ TimesTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimesTable
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 1; i <= 10; i++)
+            {
+
+                Console.WriteLine("{0} x {1} = {2}", n, i, n * i);
+            }
+
+        }
+    }
+}
