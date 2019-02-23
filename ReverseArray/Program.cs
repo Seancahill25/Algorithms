9666911de616af9b368595acd8e4034f5aa26c31
Sean Cahill <seancahill25@gmail.com>
2019-02-22 22:30:07 -0500

    Reversing an array

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Given an array,A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+namespace ReverseArray
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            Array.Reverse(arr);
+            for (var i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+        }
+    }
+}
