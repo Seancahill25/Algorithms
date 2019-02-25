ebd306017867688cf5ef5e5d98f954a0c44fb75b
Sean Cahill <seancahill25@gmail.com>
2019-02-25 10:18:14 -0500

    Using recursion to output factorial of n.

---
+++ RecursionRecursion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionRecursion
+{
+    class Solution
+    {
+        static int Factorial(int n)
+        {
+            if (n > 1)
+            {
+                return n * Factorial(n - 1);
+            }
+            else
+            {
+                return n;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = Factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
