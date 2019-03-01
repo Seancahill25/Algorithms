a114ae8400ee1bdc73bf2def978ddad636037fdc
Sean Cahill <seancahill25@gmail.com>
2019-02-28 23:45:10 -0500

    finds highest sum hourglass from 2d array

---
+++ Hourglasses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+
+namespace Hourglasses
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+            var sum = -100;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    var sumtemp = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                    if (sumtemp > sum)
+                    {
+                        sum = sumtemp;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+
+}
