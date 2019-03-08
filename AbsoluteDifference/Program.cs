23b23bdb2920bda6ee84ecb56bfba39a12831d6c
Sean Cahill <seancahill25@gmail.com>
2019-03-07 23:35:02 -0500

    finds the ablsoute difference between two numbers. practicing how I can use values within scope.

---
+++ AbsoluteDifference/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteDifference
+{
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference(int[] arr)
+        {
+            elements = arr;
+        }
+
+        public void computeDifference()
+        {
+            Array.Sort(elements);
+            maximumDifference = Math.Abs(elements[0] - elements[elements.Length - 1]);
+        }
+    } 
+
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+}
