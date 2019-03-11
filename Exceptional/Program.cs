c2f8b17be59cf0d9e4a9a1a7ecb9e1835f73487d
Sean Cahill <seancahill25@gmail.com>
2019-03-10 23:08:46 -0400

    parsing an integer from a string and printing a custom error message

---
+++ Exceptional/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptional
+{
+    class Solution
+    {
+
+        static void Main(String[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(int.Parse(S));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+
+}
