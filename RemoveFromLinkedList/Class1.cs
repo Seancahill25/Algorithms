42dfc84d8d31998407a0b4e36f43398f5a8d8518
Sean Cahill <seancahill25@gmail.com>
2019-03-14 10:44:15 -0400

    using a recursive function to remove all k elements from a linked list.

---
+++ RemoveFromLinkedList/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Definition for singly-linked list:
+// class ListNode<T> {
+//   public T value { get; set; }
+//   public ListNode<T> next { get; set; }
+// }
+
+public class Class1
+{
+	public Class1()
+	{
+        ListNode<int> removeKFromList(ListNode<int> l, int k)
+        {
+            if (l == null)
+            {
+                return null;
+            }
+            if (l.value != k)
+            {
+                l.next = removeKFromList(l.next, k);
+            }
+            else
+            {
+                l = removeKFromList(l.next, k);
+            }
+            return l;
+        }
+    }
+}
