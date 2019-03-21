d4d5d8fd0d001ae445d238422315c59afa2bbe88
Sean Cahill <seancahill25@gmail.com>
2019-03-21 10:13:51 -0400

    Takes an int and reverses the order of the digits

---
+++ ReversedOrder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(ReversedOrder(num));
+        }
+
+        public static int ReversedOrder(int num)
+        {
+            int reverse = 0;
+
+            while (num != 0)
+            {
+                reverse = reverse * 10 + num % 10;
+                num /= 10;
+            }
+            return reverse;
+        }
+    }
+}
