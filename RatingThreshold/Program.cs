b0d6e972c2a4dac471751938738d13bc4af18e91
Sean Cahill <seancahill25@gmail.com>
2019-03-27 22:46:53 -0400

    given a list of ratings, finds the array that does not pass the threshold

---
+++ RatingThreshold/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatingThreshold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double threshold = 3.5;
+            int[][] ratings = new int[][] 
+            {
+                new int[] {3,4},
+                new int[] {3,3,3,4},
+                new int[] {4}
+            };
+
+            foreach (var rating in ratingThreshold(threshold, ratings))
+            {
+                Console.WriteLine(rating);
+            }
+
+        }
+
+        public static int[] ratingThreshold(double threshold, int[][] ratings)
+        {
+            List<int> review = new List<int>();
+            double[] totals = new double[ratings.Length];
+            double math = 0;
+
+            for (var i = 0; i < ratings.Length; i++)
+            {
+                for (var j = 0; j < ratings[i].Length; j++)
+                {
+                    math += ratings[i][j];
+                }
+                math /= ratings[i].Length;
+                totals[i] = math;
+                if (totals[i] < threshold)
+                {
+                    review.Add(Array.IndexOf(totals, totals[i]));
+                }
+                math = 0;
+            }
+            return review.ToArray();
+        }
+
+    }
+}
