10a530e665fba424915669a0527fba2603ff8a3c
Sean Cahill <seancahill25@gmail.com>
2019-04-15 23:30:18 -0400

    algorithm that checks two arrays, to see if any single value from both arrays can be added to the sum of v

---
+++ SumOfTwo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            int[] b = { 10, 20, 30, 40 }; 
+            int v = 42;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (a[i] + b[j] == v)
+                    {
+                        Console.WriteLine(true);
+                    } 
+                }
+            }
+            Console.WriteLine(false);
+        }
+    }
+}
