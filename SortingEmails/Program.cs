b56d07d08e9dc87640295965bf9bd62f4123234e
Sean Cahill <seancahill25@gmail.com>
2019-04-21 22:22:46 -0400

    takes in array of names and emails, sorts emails in alphabetical order by name

---
+++ SortingEmails/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int j = 0;
+            string[] names = new string[N];
+            for (int i = 0; i < N; i++)
+            {
+                string[] tokens_firstName = Console.ReadLine().Split(' ');
+                string firstName = tokens_firstName[0];
+                string emailID = tokens_firstName[1];
+
+                if (emailID.EndsWith("@gmail.com"))
+                {
+                    names[j] = firstName;
+                    j++;
+                }
+            }
+
+            Array.Sort(names);
+            for (int k = 0; k < names.Length; k++)
+            {
+                if (names[k] != null)
+                {
+                    Console.WriteLine(names[k]);
+                }
+            }
+        }
+    }
+}
