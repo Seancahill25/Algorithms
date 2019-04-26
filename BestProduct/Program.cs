789289470db2dd3e25a97215eb2dd1306a94ec87
Sean Cahill <seancahill25@gmail.com>
2019-04-25 23:51:48 -0400

    checks array to find highest product of two adjecent numbers.

---
+++ BestProduct/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputArray = new int[]{ 3, 6, -2, -5, 7, 3 };
+                int product = int.MinValue;
+                for (int i = 0; i < inputArray.Length - 1; i++)
+                {
+                    if (inputArray[i] * inputArray[i + 1] > product)
+                    {
+                        product = inputArray[i] * inputArray[i + 1];
+                    }
+                }
+            Console.WriteLine(product);
+        }
+    }
+}
