99d75c31311185370e31d1961ec0b031d997e7f0
Sean Cahill <seancahill25@gmail.com>
2019-05-07 19:54:18 -0400

    finds how many statues are needed to make the sorted array increase exactly by 1.

---
+++ ConsecutiveArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsecutiveArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] statues = { 6, 2, 3, 8 };       
+            Console.WriteLine(makeArrayConsecutive2(statues));
+        }
+
+        public static int makeArrayConsecutive2(int[] statues)
+        {
+            return statues.Max() - statues.Min() - statues.Length + 1;
+        }
+    }
+}
+//arrange the statues from smallest to largest,so that each statue will be bigger than the previous one exactly by 1.
+//figure out the minimum number of additional statues needed.
