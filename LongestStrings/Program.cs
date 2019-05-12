71f60a591a9604b4ac1af263ee8ddc70b705122c
Sean Cahill <seancahill25@gmail.com>
2019-05-12 16:42:10 -0400

    Takes an array of strings, returns another array containing all of its longest strings.

---
+++ LongestStrings/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = { "aba", "aa", "ad", "vcd", "aba" };
+            string[] inputArray2 = { "young", "yooooooung", "hot", "or", "not", "come", "on", "fire", "water", "watermelon" };
+
+            foreach(string input in allLongestStrings(inputArray))
+            {
+                Console.WriteLine(input);
+            }
+            Console.WriteLine();
+            foreach(string input in allLongestStrings(inputArray2))
+            {
+                Console.WriteLine(input);
+            }
+        }
+
+        static string[] allLongestStrings(string[] inputArray)
+        {
+            List<string> newArray = new List<string>();
+            int stringLength = 0;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i].Length > stringLength)
+                {
+                    stringLength = inputArray[i].Length;
+                }
+            }
+            foreach (string input in inputArray)
+            {
+                if (input.Length == stringLength)
+                {
+                    newArray.Add(input);
+                }
+            }
+            return newArray.ToArray();
+        }
+
+    }
+}
