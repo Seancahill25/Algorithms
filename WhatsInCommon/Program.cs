87f1c4c2b50eb644a3982c3f58423cbe30aeb229
Sean Cahill <seancahill25@gmail.com>
2019-05-14 19:46:43 -0400

    takes two strings, finds the number of common characters between them.

---
+++ WhatsInCommon/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsInCommon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "aabcc";
+            string s2 =" adcaa";
+            string s3 = "zzzz";
+            string s4 = "zzzzzzz";
+
+            Console.WriteLine(commonCharacterCount(s1, s2));
+            Console.WriteLine(commonCharacterCount(s3, s4));
+        }
+
+        static int commonCharacterCount(string s1, string s2)
+        {
+            List<char> list1 = s1.ToList();
+            List<char> list2 = s2.ToList();
+            int count = 0;
+
+            foreach (char c in list2)
+            {
+                if (list1.Contains(c))
+                {
+                    list1.Remove(c);
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
